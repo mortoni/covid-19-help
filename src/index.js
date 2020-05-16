8bcc0248884cd2dfb0ed64c46a29e454a4a48732
Alan Mortoni <alan_mortoni@flightcentre.com>
2020-05-16 20:13:12 +1000

    refactor

--- src/index.js
+++ src/index.js
@@ -1,12 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import Dashboard from './components/DashBoard'
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Dashboard />
   </React.StrictMode>,
   document.getElementById('root')
 );
