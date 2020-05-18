ff1d52868fa3a8877c5bc7d7d87ec6568bab6a9d
Alan Mortoni <alan_mortoni@flightcentre.com>
2020-05-18 19:33:50 +1000

    added: build

---
+++ build/precache-manifest.570499abb4bb5b4091c33e1a099e6c4f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "709c033f35f960ad4db68d54fdb28d66",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1f7d510e5e12979929ca",
+    "url": "/static/js/2.88fa00fd.chunk.js"
+  },
+  {
+    "revision": "3a08a7c8f8e6231522abe29a929e6912",
+    "url": "/static/js/2.88fa00fd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "457bdfbdbcd155dc381b",
+    "url": "/static/js/main.16182fa6.chunk.js"
+  },
+  {
+    "revision": "81a156cf3493c3149845",
+    "url": "/static/js/runtime-main.dec72f34.js"
+  }
+]);
