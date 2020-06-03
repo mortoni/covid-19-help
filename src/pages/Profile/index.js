64efe62f390f686c0e057eab7a23419e6ab79174
Alan Mortoni <alan_mortoni@flightcentre.com>
2020-06-03 18:18:51 +1000

    fixed: Profile page style

--- src/pages/Profile/index.js
+++ src/pages/Profile/index.js
@@ -1,13 +1,10 @@
 import React from 'react'
-import { Box } from '@material-ui/core'
+import { Box, Container, Grid, Typography, Divider, Button } from '@material-ui/core'
 import { makeStyles } from '@material-ui/core/styles'
+import LocationIcon from '@material-ui/icons/LocationOnOutlined'
+import { useAuth } from 'context/auth-context'
 import Avatar from 'components/Avatar'
-import Location from './components/Location'
-import Badge from './components/Badge'
 import Status from './components/Status'
-import Information from './components/Information'
-import About from './components/About'
-import Posts from './components/Posts'
 
 const useStyles = makeStyles((theme) => ({
   content: {
@@ -28,34 +25,65 @@ const useStyles = makeStyles((theme) => ({
     left: `calc(50% - ${theme.spacing(12 / 2)}px)`,
     position: 'absolute',
   },
+  root: {
+    backgroundColor: theme.palette.grey[200],
+    height: '100%',
+  },
+  container: {
+    backgroundColor: 'white',
+    height: '100%',
+  },
 }))
 const ProfilePage = () => {
+  const { user } = useAuth()
   const classes = useStyles()
 
   return (
-    <>
-      <Box className={classes.content}>
-        <Box display="flex" flexGrow={1}>
-          <Box className={classes.avatar} position="absolute">
-            <Avatar size={12} />
-          </Box>
-
-          <Box mt={7} display="flex" flexDirection="column" flexGrow={1} alignItems="center">
-            <Location />
-            <Box my={1}>
-              <Badge />
-              <Status />
+    <Box pt={{ xs: 0, sm: 3 }} className={classes.root}>
+      <Container maxWidth="sm" component="main" className={classes.container}>
+        <Grid container spacing={2}>
+          <Grid item xs={12} container direction="column">
+            <Box my={2} textAlign="center">
+              <Typography variant="h5">Profile</Typography>
             </Box>
+            <Divider />
+          </Grid>
 
-            <About />
-            <Information />
-          </Box>
-        </Box>
-        <Box className={classes.postContent}>
-          <Posts />
-        </Box>
-      </Box>
-    </>
+          <Grid item xs={12}>
+            <Box my={3} display="flex" alignItems="center" flexDirection="column">
+              <Avatar size={12}>{`${user.firstName.charAt(0)} ${user.lastName.charAt(0)}`}</Avatar>
+              <Box my={2}>
+                <Typography variant="h4">{`${user.firstName} ${user.lastName}`}</Typography>
+              </Box>
+              <Box display="flex">
+                <Box mr={2}>
+                  <LocationIcon color="secondary" />
+                </Box>
+                <Typography variant="body1">{user.address.location}</Typography>
+              </Box>
+            </Box>
+          </Grid>
+
+          <Grid item xs={12}>
+            <Status />
+          </Grid>
+
+          <Grid item xs={12}>
+            <Box my={2} px={{ xs: 0, md: 5 }}>
+              <Box display="flex" justifyContent="space-between">
+                <Typography variant="h6">About</Typography>
+                <Button color="primary" onClick={() => {}}>
+                  Edit
+                </Button>
+              </Box>
+              I’m 32 years old, healthy. I have a car and am more than happy to help when it comes to running errands.
+              However, I have 4 kids and a dog am quite busy on a general basis. I would be happy if anyone could help
+              me walk my dogs whenever possible.
+            </Box>
+          </Grid>
+        </Grid>
+      </Container>
+    </Box>
   )
 }
 
